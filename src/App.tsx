53d7e691d89e803c4b92dc8b093860116c0e09e3
Wonseok <dnjstjr970302@gmail.com>
2024-04-29 16:44:08 +0900

    'feat/리액트 쿼리로 ipdata 관리'

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useIpinfo from './api/Ipinfo';
 
 function App() {
-  return <>Hello World</>;
+  const { data, isLoading, isError } = useIpinfo();
+
+  if (isLoading) return <div>로딩</div>;
+  if (isError) return <div>에러</div>;
+  if (!data) return <div>데이터없음</div>;
+
+  return (
+    <>
+      <h1>IP Information</h1>
+      <p>IP: {data.ip}</p>
+      <p>Country: {data.country}</p>
+    </>
+  );
 }
 
 export default App;
--- src/api/Ipinfo.ts
+++ src/api/Ipinfo.ts
@@ -1,22 +1,37 @@
 import axios from 'axios';
+import { useQuery } from 'react-query';
 
 export interface IpInfoData {
   ip: string;
   country: string;
 }
 
-const APIKEY = process.env.REACT_APP_IPINFO;
-export const fetchIpinfo = async (): Promise<IpInfoData> => {
-  try {
-    const ipInfoResponse = await axios.get(
-      `https://ipinfo.io/json?token=${APIKEY}`
-    );
-    console.log('ipInfoResponse:', ipInfoResponse.data);
-    const ip = ipInfoResponse.data.ip;
-    const userCountry = ipInfoResponse.data.country;
-    return { ip, country: userCountry };
-  } catch (error) {
-    console.error('데이터를 가져오는 중 오류가 발생했습니다.', error);
-    throw error;
-  }
+const fetchIpinfo = async () => {
+  const APIKEY = process.env.REACT_APP_IPINFO;
+  const ipInfoResponse = await axios.get(
+    `https://ipinfo.io/json?token=${APIKEY}`
+  );
+  return ipInfoResponse.data;
 };
+
+const useIpinfo = () => {
+  return useQuery<IpInfoData>('ipinfo', fetchIpinfo);
+};
+
+export default useIpinfo;
+
+// const APIKEY = process.env.REACT_APP_IPINFO;
+// export const fetchIpinfo = async (): Promise<IpInfoData> => {
+//   try {
+//     const ipInfoResponse = await axios.get(
+//       `https://ipinfo.io/json?token=${APIKEY}`
+//     );
+//     console.log('ipInfoResponse:', ipInfoResponse.data);
+//     const ip = ipInfoResponse.data.ip;
+//     const userCountry = ipInfoResponse.data.country;
+//     return { ip, country: userCountry };
+//   } catch (error) {
+//     console.error('데이터를 가져오는 중 오류가 발생했습니다.', error);
+//     throw error;
+//   }
+// };
